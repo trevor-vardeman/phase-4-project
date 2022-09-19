70d4a73d7c172f0f7cf39e9f94d5e64dabc2c97f
Trevor Vardeman <trevortx@gmail.com>
2022-09-19 00:24:36 -0700

    deleted unnecessary files and added navbar

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navigation from './Navigation'
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Navigation.js
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+function Navigation() {
+  return (
+    <Navbar sticky="top" bg="dark" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="#home">Brand Name</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#all">All</Nav.Link>
+          <Nav.Link href="#my-communities">My Communities</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#new-community">New Community</Nav.Link>
+          <Nav.Link href="#new-post">New Post</Nav.Link>
+          <Nav.Link href="#sign-in">Sign In</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Navigation
