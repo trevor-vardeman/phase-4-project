ef48a6977c64e7b2658d59d1359c5a1a83a3180b
Trevor Vardeman <trevortx@gmail.com>
2022-10-10 20:03:56 -0700

    fixed a bug that was happening when a user wasn't signed in

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,8 +1,12 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :text, :image_url, :points, :created_at
+  attributes :id, :title, :text, :image_url, :points, :created_at, :user_can_modify
 
   belongs_to :user
   belongs_to :community
   has_many :comments
   has_many :users, through: :comments
+
+  def user_can_modify
+    current_user.try(:admin) || current_user == self.object.user && current_user.id != nil
+  end
 end
